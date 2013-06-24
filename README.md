b3bca537d91ded126317eb93d9d3870bffb34c53
Robert Martin <robertmartinbc@shaw.ca>
2013-06-24 15:35:37 -0700

    First commit and README update.

---
+++ README.md
@@ -0,0 +1,3 @@
+A Reddit-style application to demonstrate foundation programming and web development skills.
+
+Made with my loving cohort at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloccit::Application.config.secret_token = '457075f39f947861471ee0bfba9968cab793233f424d91bc4df4c0ae504c578eb07d7d807be63d5330f8bd588acdbfd36d0dea7fc434a84d7f6cc722ef0bdfad'
