7e319c5c345100e209089859def9fecf0eafa371
Robert Martin <robertmartinbc@shaw.ca>
2013-06-25 16:49:15 -0700

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-Bloccit::Application.routes.draw do
+Bloccit::Application.routes.draw do  
 
   devise_for :users
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   
 
